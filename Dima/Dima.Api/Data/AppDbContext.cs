d47245438e88e5cf9495a335c5b83cafc2be0e0f
Alexandre Rodrigues <ale.rodrigues@outlook.com>
2025-09-06 00:31:29 -0300

    Entity Framework
    015 Acesso a Dados
    016 Entity Framework
    017 DbContext e DbSet

---
+++ Dima/Dima.Api/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Dima.Core.Models;
+
+namespace Dima.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Category> Categories { get; set; } = null!;
+    public DbSet<Transaction> Transactions { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) 
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfiguration();
+    }
+}
