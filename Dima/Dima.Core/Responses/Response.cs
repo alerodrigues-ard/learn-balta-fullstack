162bfaeade0249489c98cd0dc0d3bc8fdf13c3a4
Alexandre Rodrigues <ale.rodrigues@outlook.com>
2025-09-19 23:56:57 -0300

    Padronização
    031 Construtores e Serialização

--- Dima/Dima.Core/Responses/Response.cs
+++ Dima/Dima.Core/Responses/Response.cs
@@ -1,18 +1,31 @@
-﻿namespace Dima.Core.Responses;
+﻿using System.Text.Json.Serialization;
 
-public class Response
+namespace Dima.Core.Responses;
+
+public class Response<TData>
 {
+    private const int DefaultStatusCode = 200;
+    
     private readonly int _code;
     
-    public string? Data { get; set; }
-    public string Message { get; set; } =  string.Empty;
+    public TData? Data { get; set; }
+    public string? Message { get; set; }
 
-    public Response(string? data, string message, int code)
+    // Construtor sem parâmetros necessário para funcionamento correto do .NET
+    // na serialização
+    [JsonConstructor]
+    public Response()
+    {
+        _code = DefaultStatusCode;
+    }
+
+    public Response(TData? data, int code = DefaultStatusCode, string? message = null)
     {
         Data = data;
         Message = message;
         _code = code;
     }
 
+    [JsonIgnore]
     public bool IsSuccess => _code is >= 200 and <= 299;
 }
