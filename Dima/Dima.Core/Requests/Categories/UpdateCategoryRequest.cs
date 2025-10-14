46bb1d22fd239b04800fbe147483de74fe7d9866
Alexandre Rodrigues <ale.rodrigues@outlook.com>
2025-10-13 23:41:40 -0300

    Handlers
    039 Atualizando a Categoria - Handler

--- Dima/Dima.Core/Requests/Categories/UpdateCategoryRequest.cs
+++ Dima/Dima.Core/Requests/Categories/UpdateCategoryRequest.cs
@@ -4,6 +4,8 @@ namespace Dima.Core.Requests.Categories;
 
 public class UpdateCategoryRequest : Request
 {
+    public long Id { get; set; }
+    
     [Required(ErrorMessage = "Título inválido")]
     [MaxLength(ErrorMessage = "O título deve conter até 80 caracteres")]
     public string Title { get; set; } = string.Empty;
