1b9c906dd32e06859b208e7c95b5c802de72d26b
Alexandre Rodrigues <ale.rodrigues@outlook.com>
2025-10-14 23:34:15 -0300

    Endpoints
    046 Segmentando os Endpoints

--- Dima/Dima.Api/Endpoints/Categories/CreateCategoryEndpoint.cs
+++ Dima/Dima.Api/Endpoints/Categories/CreateCategoryEndpoint.cs
@@ -1,11 +1,27 @@
 ﻿using Dima.Api.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Responses;
 
 namespace Dima.Api.Endpoints.Categories;
 
 public class CreateCategoryEndpoint : IEndpoint
 {
     public static void Map(IEndpointRouteBuilder app)
+        => app.MapPost("/", HandleAsync)
+            .Produces<Response<Category?>>();
+
+    private static async Task<IResult> HandleAsync(
+        ICategoryHandler handler,
+        CreateCategoryRequest request)
     {
-        throw new NotImplementedException();
+        var result = await handler.CreateAsync(request);
+
+        if (result.IsSuccess)
+            return Results.Created($"/{result?.Data?.Id}", result);
+        
+        return Results.BadRequest(result.Data);
     }
+ 
 }
