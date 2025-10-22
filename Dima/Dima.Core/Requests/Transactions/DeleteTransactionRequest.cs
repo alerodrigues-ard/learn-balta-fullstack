3e530ec5ccf1adcca6bcbf12e31e85bc4e1886c9
Alexandre Rodrigues <ale.rodrigues@outlook.com>
2025-10-21 22:58:38 -0300

    API de Transações
    050 Requests - Transaction

---
+++ Dima/Dima.Core/Requests/Transactions/DeleteTransactionRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Dima.Core.Requests.Transactions;
+
+public class DeleteTransactionRequest : Request
+{
+    
+    public long Id { get; set; }
+    
+}
---
+++ Dima/Dima.Core/Requests/Transactions/GetTransactionsByPeriordRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Dima.Core.Requests.Transactions;
+
+public class GetTransactionsByPeriordRequest : PagedRequest
+{
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+}
