a66dfdffc826e4727230c09076ebd13cb0ea3510
Alexandre Rodrigues <ale.rodrigues@outlook.com>
2025-11-04 00:08:02 -0300

    ASP.NET Identity
    059 IdentityDbContext

--- Dima/Dima.Api/Data/AppDbContext.cs
+++ Dima/Dima.Api/Data/AppDbContext.cs
@@ -1,11 +1,24 @@
 ﻿using System.Reflection;
+using Dima.Api.Models;
 using Microsoft.EntityFrameworkCore;
 using Dima.Core.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Dima.Api.Data;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext(options)
+public class AppDbContext(DbContextOptions<AppDbContext> options) 
+    : IdentityDbContext
+    <
+        User,                       // Tabela AspNetUsers 
+        IdentityRole<long>,         // Tabela AspNetRoles (gera tabela de ligação AspNetUserRoles)
+        long,
+        IdentityUserClaim<long>,    // Tabela AspNetUserClaims
+        IdentityUserRole<long>,     // Tabela AspNetUserRoles
+        IdentityUserLogin<long>,    // Tabela AspNetUserLogins
+        IdentityRoleClaim<long>,    // Tabela AspNetRoleClaims
+        IdentityUserToken<long>     // Tabela AspNetUserTokens
+    >(options)
 {
     public DbSet<Category> Categories { get; set; } = null!;
     public DbSet<Transaction> Transactions { get; set; } = null!;
