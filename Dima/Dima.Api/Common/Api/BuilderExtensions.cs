2afb5ac9faf9b58b8af3e928f58b4173a41fbbf9
Alexandre Rodrigues <ale.rodrigues@outlook.com>
2026-03-27 00:20:32 -0300

    Organizando o Minimal API
    068 Extensions Methods

--- Dima/Dima.Api/Common/Api/BuilderExtensions.cs
+++ Dima/Dima.Api/Common/Api/BuilderExtensions.cs
@@ -1,4 +1,10 @@
-﻿using Dima.Core;
+﻿using Dima.Api.Data;
+using Dima.Api.Handlers;
+using Dima.Api.Models;
+using Dima.Core;
+using Dima.Core.Handlers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Dima.Api.Common.Api;
 
@@ -12,5 +18,63 @@ public static class BuilderExtensions
             .GetConnectionString("DefaultConnection") 
             ?? string.Empty;
     }
-    
+
+    public static void AddDocumentation(this WebApplicationBuilder builder)
+    {
+        // Swagger
+        // OpenAPI nativo .NET 10
+        builder.Services.AddEndpointsApiExplorer(); // Necessário para o Swagger
+        builder.Services.AddOpenApi();
+        
+        //Document(document =>
+        //{
+        //    document.DocumentName = "v1";
+        //    document.Title = "Dima API";
+        //    document.Version = "v1.0.0";
+        //    document.Description = "API Dima - .NET 10";
+        //});
+        /*
+        builder.Services.AddEndpointsApiExplorer(); // Necessário para o Swagger
+        builder.Services.AddSwaggerGen( // Necessário para o Swagger
+            x =>
+            {
+                x.CustomSchemaIds(type => type.FullName ?? type.Name); // Necessário para o Swagger colocar o Full Qualified Name
+            });
+        */
+    }
+
+    public static void AddSecurity(this WebApplicationBuilder builder)
+    {
+        // A ordem das duas chamadas abaixo deve ser mantida
+        builder.Services                                                // Quem você é?
+            .AddAuthentication(IdentityConstants.ApplicationScheme)
+            .AddIdentityCookies();
+        builder.Services.AddAuthorization();        // O que você pode fazer?
+    }
+
+    public static void AddDataContexts(this WebApplicationBuilder builder)
+    {
+        builder.Services
+            .AddIdentityCore<User>() // Classe User customizada - O padrão é IdentityUser
+            .AddRoles<IdentityRole<long>>() // IdentityRole padrão usando long
+            .AddEntityFrameworkStores<AppDbContext>() // Armazenamento dos dados de identity
+            .AddApiEndpoints(); // Cadastro padrão de endpoints da API
+
+        builder.Services.AddDbContext<AppDbContext>(x =>
+        {
+            x.UseSqlServer(Configuration.ConnectionString);
+        });
+    }
+
+    public static void AddCrossOrigin(this WebApplicationBuilder builder)
+    {
+        
+    }
+
+    public static void AddServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
+
+        builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
+    }
 }
--- Dima/Dima.Api/Program.cs
+++ Dima/Dima.Api/Program.cs
@@ -1,61 +1,14 @@
-using System.Security.Claims;
 using Dima.Api.Common.Api;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Dima.Api.Data;
-using Dima.Api.Handlers;
-using Dima.Api.Models;
-using Dima.Core.Handlers;
 using Dima.Api.Endpoints;
-using Dima.Core;
-using Microsoft.OpenApi;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddConfiguration();
-
-// Swagger
-// OpenAPI nativo .NET 10
-builder.Services.AddEndpointsApiExplorer(); // Necessário para o Swagger
-builder.Services.AddOpenApi();
-//Document(document =>
-//{
-//    document.DocumentName = "v1";
-//    document.Title = "Dima API";
-//    document.Version = "v1.0.0";
-//    document.Description = "API Dima - .NET 10";
-//});
-/*
-builder.Services.AddEndpointsApiExplorer(); // Necessário para o Swagger
-builder.Services.AddSwaggerGen( // Necessário para o Swagger
-    x =>
-    {
-        x.CustomSchemaIds(type => type.FullName ?? type.Name); // Necessário para o Swagger colocar o Full Qualified Name
-    });
-*/
-
-// A ordem das duas chamadas abaixo deve ser mantida
-builder.Services                                                // Quem você é?
-    .AddAuthentication(IdentityConstants.ApplicationScheme)
-    .AddIdentityCookies();
-builder.Services.AddAuthorization();        // O que você pode fazer?
-
-builder.Services
-    .AddIdentityCore<User>() // Classe User customizada - O padrão é IdentityUser
-    .AddRoles<IdentityRole<long>>() // IdentityRole padrão usando long
-    .AddEntityFrameworkStores<AppDbContext>() // Armazenamento dos dados de identity
-    .AddApiEndpoints(); // Cadastro padrão de endpoints da API
-
-builder.Services.AddDbContext<AppDbContext>(x =>
-{
-    x.UseSqlServer(Configuration.ConnectionString);
-});
-
-
-
-builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
-
-builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
+builder.AddSecurity();
+builder.AddDataContexts();
+builder.AddCrossOrigin();
+builder.AddDocumentation();
+builder.AddServices();
 
 var app = builder.Build();
 
