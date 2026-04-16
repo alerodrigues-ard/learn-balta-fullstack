0f3364d9667291f8c2c2f03c8052c6a96ce69ccd
Alexandre Rodrigues <ale.rodrigues@outlook.com>
2026-04-16 14:30:26 -0300

    Identity no Blazor
    095 Cookie Authentication State Provider

---
+++ Dima/Dima.Web/Security/CookieAuthenticationStateProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Dima.Web.Security;
+
+public class CookieAuthenticationStateProvider(IHttpClientFactory clientFactory) : AuthenticationStateProvider, ICookieAuthenticationStateProvider
+{
+    private bool _isAuthenticated = false;
+    private readonly HttpClient _client = clientFactory.CreateClient(Configuration.HttpClienName);
+    
+    public async Task<bool> CheckAuthenticatedAsync()
+    {
+        await GetAuthenticationStateAsync();
+        return _isAuthenticated;
+    }
+
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void NotifyAuthenticationStateChanged()
+        => NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+}
