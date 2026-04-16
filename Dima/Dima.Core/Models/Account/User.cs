02a76046eaf031a53e1d7ba80a45fd9cf01b4a96
Alexandre Rodrigues <ale.rodrigues@outlook.com>
2026-04-16 14:41:13 -0300

    Identity no Blazor
    096 Claims

--- Dima/Dima.Core/Models/Account/User.cs
+++ Dima/Dima.Core/Models/Account/User.cs
@@ -4,5 +4,6 @@ public class User
 {
     // Dados da Url padrão do Identity v1/identity/manage/info
     public string Email { get; set; } = string.Empty;
+    public bool IsEmailConfirmed { get; set; }
     public Dictionary<string, string> Claims { get; set; } = [];
 }
--- Dima/Dima.Web/Security/CookieAuthenticationStateProvider.cs
+++ Dima/Dima.Web/Security/CookieAuthenticationStateProvider.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
+﻿using System.Net.Http.Json;
+using System.Security.Claims;
+using Dima.Core.Models.Account;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace Dima.Web.Security;
 
@@ -15,7 +18,30 @@ public class CookieAuthenticationStateProvider(IHttpClientFactory clientFactory)
 
     public override Task<AuthenticationState> GetAuthenticationStateAsync()
     {
-        throw new NotImplementedException();
+        
+    }
+
+    private async Task<User?> GetUser()
+    {
+        try
+        {
+            return await _client.GetFromJsonAsync<User?>("v1/identity/manage/info");
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+    }
+
+    private async Task<List<Claim>> GetClaims(User user)
+    {
+        var claims = new List<Claim>()
+        {
+            new(ClaimTypes.Name, user.Email), 
+            new(ClaimTypes.Email, user.Email) 
+        };
+
+        return claims;
     }
 
     public void NotifyAuthenticationStateChanged()
