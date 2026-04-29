4c1d138637a81766808465feeaf5f0e8259238fd
Alexandre Rodrigues <ale.rodrigues@outlook.com>
2026-04-29 14:33:32 -0300

    Identity no Blazor
    101 Resolvendo dependências

--- Dima/Dima.Web/Program.cs
+++ Dima/Dima.Web/Program.cs
@@ -3,12 +3,24 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using Dima.Web;
 using Dima.Web.Security;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+Configuration.BackendUrl = builder.Configuration.GetValue<string>("BackendUrl") ?? string.Empty;
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<CookieHandler>();
+
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped<AuthenticationStateProvider, CookieAuthenticationStateProvider>();
+
+// O cast para ICookAuthenticationStateProvider, pois, apesar da injeção de dependência acima, 
+// o .NET pode acabar tentando usar uma instância padrão de AuthenticationStateProvicer
+builder.Services.AddScoped(x => (ICookieAuthenticationStateProvider)x.GetRequiredService<AuthenticationStateProvider>());
 
 builder.Services.AddMudServices();
 
